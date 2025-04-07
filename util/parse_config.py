71f8fc8eb27251b6cdfe50b961ddcf597152516f
Christina Asante <1641788534113305@mil>
2025-04-07 00:37:09 -0400

    manual url parsing to generate config: still buggy

--- util/parse_config.py
+++ util/parse_config.py
@@ -1,7 +1,13 @@
+import os
+import re
+import time
 import yaml
 import pandas as pd
 import ast
 import json
+import sys 
+from urllib.parse import unquote
+from datetime import datetime
 
 def print_formatted_dict(dict_obt : dict, indent = 0) -> None:
     for key, value in dict_obt.items():
@@ -147,6 +153,183 @@ class ParseConfig:
         return True
             
             
+    def parse_erddap_url() -> None:
+        """
+        Populates configuration file based on existing URL
+        
+        """
+        url = sys.argv[1]
+        
+        query_url = url.split("?")[-1]
+        base_url_list = url.split("/")[0:-1]
+        dataset_name = url.split("/")[-1]
+        dataset_name = dataset_name.split("?")[0]
+        base_url = "/".join(base_url_list)
+
+        print(base_url_list)
+        print(dataset_name)
+        print(base_url)
+        print(query_url)
+
+        dap_type = 0
+
+        if 'tabledap' in base_url:
+            dap_type = 'tabledap'
+            dataset_params = ParseConfig.extract_erddap_table_params(query_url)
+        elif 'griddap' in base_url:
+            dap_type = 'griddap'
+            dataset_params = ParseConfig.extract_erddap_grid_params(query_url)
+
+        return {
+                'base_url' : base_url,
+                'dap_type' : dap_type,
+                'dataset_id' : dataset_name,
+                'full_url' : url,
+                'constraints' : dataset_params
+        }
+
+        
+    def populate_config(args : dict) -> None:
+
+
+        with open('../erddap-griddap-config.yaml', 'r') as file:
+            data = yaml.safe_load(file)
+        print(data)
+
+        data['server']['url'] = args['base_url']
+        data['server']['dap_type'] = args['dap_type']
+        data['datasets']['name'] = args['dataset_id']
+        data['datasets']['dataset_url'] = args['full_url']
+        # dataset constraints
+        data['datasets']['variables'][0]['name'] = args['constraints']['dataset_variables']
+        data['datasets']['variables'][0]['dimensions'] = ['time', 'latitude', 'longitude']
+        data['datasets']['variables'][0]['range']['time'] = args['constraints']['time']
+        data['datasets']['variables'][0]['range']['lat'] = args['constraints']['latitude']
+        data['datasets']['variables'][0]['range']['long'] = args['constraints']['longitude']
+
+        if args['dap_type'] == 'griddap':
+            data['query']['output_format'] = "nc"
+        else:
+            data['query']['output_format'] = "csv"
+
+        with open(f'erddap-{args['dap_type']}-config-{time.localtime()}.yaml', 'w') as new_file:
+            yaml.dump(data, new_file)
+            
+    
+    def extract_erddap_table_params(query : str) -> None:
+        """
+        Extrapolates config parameters to manually generate config given an erddap tabledap URL
+
+        query : The parameter extensions of the ERDDAP URL
+
+        returns:
+        None
+        """
+        # NOTE: Currently does not return dataset variables
+        params = query.split("&")[0]
+        ranges = query.split("&")[1:]
+
+        range_dict = {}
+        for r in ranges:
+
+            r = unquote(r)
+            current_param = r.split('<>=<=>=')[0]
+            print(current_param)
+            range_val = 0
+            if '>=' in r:
+               range_val = r.split(">=")[1]
+               current_param = r.split(">=")[0]
+            elif '<=' in r:
+               range_val = r.split("<=")[1]
+               current_param = r.split("<=")[0]
+            elif '=' in r:
+               range_val = r.split("=")[1]
+               current_param = r.split("=")[0]
+
+
+            if current_param in range_dict:
+                if current_param == 'latitude':
+                    range_val = float(range_val)
+                    if range_dict['latitude'][0] >= range_val:
+                        range_dict[current_param].append(range_val)
+                    else:
+                        range_dict[current_param].insert(0,range_val)
+                if current_param == 'time':
+                    print(f"this is the current time{range_dict[current_param]}")
+                    print(f"this is the current compared time: {range_val}") 
+                    #if datetime.strptime(range_dict[current_param][0], "%Y-%m-%dT%H:%M:%SZ") >= datetime.strptime(range_val, "%Y-%m-%dT%H:%M:%SZ"):
+                    if datetime.strptime(range_dict[current_param][0], "%Y-%m-%d") >= datetime.strptime(range_val, "%Y-%m-%dT%H:%M:%SZ"):
+                    
+                        date = datetime.strptime(range_val, "%Y-%m-%dT%H:%M:%SZ")
+                        range_val = date.strftime("%Y-%m-%d")
+                        print(f"this is the range_val: {range_val}")
+
+                        range_dict[current_param].insert(0, range_val)
+                    else: 
+                        date = datetime.strptime(range_val, "%Y-%m-%dT%H:%M:%SZ")
+                        range_val = date.strftime("%Y-%m-%d")
+                        print(f"this is the range_val: {range_val}")
+                        range_dict[current_param].append(range_val)
+                if current_param == 'longitude':
+                    range_val = float(range_val)
+                    if range_dict[current_param][0] >= range_val:
+                        range_dict[current_param].insert(0, range_val)
+                    else:
+                        range_dict[current_param].append(range_val)
+            else:
+                if current_param == 'time':
+                    date = datetime.strptime(range_val, "%Y-%m-%dT%H:%M:%SZ")
+                    range_val = date.strftime("%Y-%m-%d")
+                elif current_param == 'latitude' or current_param == 'longitude':
+                    range_val = float(range_val)
+    
+
+                range_dict[current_param] = [range_val]
+
+        range_dict['dataset_variables'] = unquote(params).split(',')
+        print(range_dict)
+        return range_dict
+
+
+    def extract_erddap_grid_params(query : str) -> None:
+        """
+        Extrapolates config parameters to manually generate config given an erddap griddap URL
+
+        query : The griddap compatible parameter extensions of the ERDDAP URL
+
+        returns:
+        None
+        """
+        print(unquote(query))
+
+        query = unquote(query)
+        
+        vars = re.match(r'^[^\[]+', query).group()
+        constraints = re.findall(r'\[([^\]]+)\]', query)
+
+        constraint_pairs = []
+        constraint_list = []
+
+        for x in constraints:
+            constraint_list = re.findall(r'\((.*?)\)', x)
+            if len(constraint_list) == 2:
+                constraint_pairs.append(constraint_list)
+
+
+        # this will always be ordered as time, altitude, latitude, longitude
+
+        param_dict = dict(zip(['time', 'altitude', 'latitude', 'longitude'], constraint_pairs))
+        param_dict['dataset_variables'] = vars
+        print(param_dict)
+        return param_dict
+
+
+
+
+
+
+
+        
 
 
 
@@ -155,4 +338,8 @@ class ParseConfig:
 
 
 
+if __name__ == '__main__':
+    x = ParseConfig.parse_erddap_url()
+    ParseConfig.populate_config(x)
 
+    
